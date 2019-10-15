599d8f7660a54ba5aa01e176ea74d97ddd92ea87
Inuasuraz <kiatsiri_4046@hotmail.co.th>
2019-10-15 21:50:13 +0700

    Create All Fragment and Binding Main->Menu

---
+++ app/src/main/java/buu/informatics/s59160081/projectterm_4/MainFragment.kt
@@ -0,0 +1,38 @@
+package buu.informatics.s59160081.projectterm_4
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import buu.informatics.s59160081.projectterm_4.databinding.FragmentMainBinding
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class MainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        val binding = DataBindingUtil.inflate<FragmentMainBinding>(inflater,
+            R.layout.fragment_main,container,false)
+
+        binding.apply{
+            OKButton.setOnClickListener { findNavController()
+                .navigate(R.id.action_mainFragment_to_menuFragment) }
+        }
+
+
+        return binding.root
+    }
+
+
+}
---
+++ app/src/main/java/buu/informatics/s59160081/projectterm_4/MenuFragment.kt
@@ -0,0 +1,29 @@
+package buu.informatics.s59160081.projectterm_4
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import buu.informatics.s59160081.projectterm_4.databinding.FragmentMenuBinding
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class MenuFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val binding = DataBindingUtil.inflate<FragmentMenuBinding>(inflater,
+            R.layout.fragment_menu,container,false)
+
+        return binding.root
+    }
+
+
+}
